6150cadc76c192868469353cb0c909f9a6a6439d
iamdealer02 <118016296+iamdealer02@users.noreply.github.com>
2023-07-10 11:34:26 +0200

    New changed

---
+++ main.js
@@ -0,0 +1,95 @@
+        //Division for the Contact Page----------------------------------------------------------------------------------
+        var hiddenDivision = document.getElementById("contactspage");
+        var homediv = document.getElementById("home");
+        function contacts_block() {
+
+            if (hiddenDivision.style.visibility === "hidden") {
+                hiddenDivision.style.visibility = "visible";
+                homediv.style.filter = 'blur(4px)';
+                console.log('here')
+            } else {
+                hiddenDivision.style.visibility = "hidden";
+                homediv.style.filter = 'none';
+                console.log('else')
+
+            }
+        };
+
+        //to cross the contact us pop up tab when clicking the image---------------------------------------------------------
+
+        document.getElementById("contact-link").addEventListener("click", contacts_block)
+        document.getElementById("crossimg").addEventListener("click", contacts_block)
+        // get the element to animate
+        var element = document.getElementById('characterstic');
+        var elementHeight = element.clientHeight;
+
+        // listen for scroll event and call animate function
+        document.addEventListener('scroll', animate);
+
+        // check if element is in view
+        function inView() {
+        // get window height
+        var windowHeight = window.innerHeight;
+        // get number of pixels that the document is scrolled
+        var scrollY = window.scrollY || window.pageYOffset;
+        
+        // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+        var scrollPosition = scrollY + windowHeight;
+        // get element position (distance from the top of the page to the bottom of the element)
+        var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
+        console.log(scrollPosition)
+        console.log(elementPosition)
+        
+        // is scroll position greater than element position? (is element in view?)
+        if (scrollPosition > elementPosition - 200) {
+            return true;
+        }
+        
+        return false;
+        }
+
+
+//---------------------------------------------------------------------------------------------------------
+        // animate element when it is in view
+        function animate() {
+        // is element in view?
+        if (inView()) {
+            // element is in view, add class to element
+            var circles = document.getElementsByClassName("circle");
+            for (var i = 0; i < circles.length; i++) {
+            circles[i].classList.add('animate');
+            circles[i].style.animationDelay = i*0.5 + "s";
+            
+        }
+        } else {
+            var circles = document.getElementsByClassName("circle");
+            for (var i = 0; i < circles.length; i++) {
+            circles[i].classList.remove('animate');
+            }
+            
+        }
+        }
+
+        //to sscroll to the particular part of the page--------------------------------------------------------------
+        textElement = document.getElementById('aboutus_icon');
+        textElement.addEventListener('click', function() {
+            var targetDivision = document.getElementById('aboutus');
+            targetDivision.scrollIntoView({ behavior: 'smooth' });
+        });
+
+        textElement1 = document.getElementById('homeicon');
+        textElement1.addEventListener('click', function() {
+            var targetDivision = document.getElementById('home');
+            targetDivision.scrollIntoView({ behavior: 'smooth' });
+        });
+        textElement2 = document.getElementById('serviceicon');
+        textElement2.addEventListener('click', function() {
+            var targetDivision = document.getElementById('service');
+            targetDivision.scrollIntoView({ behavior: 'smooth' });
+        });
+
+        //-------------------------------------------------------------------------------------------------------
+
+        // window.addEventListener('scroll', function(){            to shrink taskbar when you scroll down and expanded only when you are on top of your page
+        //     var 
+        // })
